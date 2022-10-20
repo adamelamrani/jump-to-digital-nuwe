ff74c375e10d293c06251f1c8fdc14635b1678b4
Adam El Amrani <adelamco1091@outlook.com>
2022-10-20 14:27:15 +0200

    Create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface ButtonProps {
+  classType: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ classType }) => {
+  return <button></button>;
+};
+
+export default Button;
