20f4a940686115f8fe6cc2e92a8f0f798d28fb27
Adam El Amrani <adelamco1091@outlook.com>
2022-10-20 14:30:05 +0200

    Import text from buttonTextSelector

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { buttonTextSelector } from "../utils/buttonTextSelector";
 
 interface ButtonProps {
   classType: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ classType }) => {
-  return <button></button>;
+  const text = buttonTextSelector(classType);
+  return <button>{text}</button>;
 };
 
 export default Button;
