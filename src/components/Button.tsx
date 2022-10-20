072b166ac8b811320cd6e67a074ca02654d55390
Adam El Amrani <adelamco1091@outlook.com>
2022-10-20 18:35:52 +0200

    Move Modal to MainPage and passsetState as prop to children

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,11 +4,16 @@ import "./styles/buttonStyle.css";
 
 interface ButtonProps {
   classType: string;
+  actionOnClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ classType }) => {
+const Button: React.FC<ButtonProps> = ({ classType, actionOnClick }) => {
   const text = buttonTextSelector(classType);
-  return <button className={`button_${classType}`}>{text}</button>;
+  return (
+    <button className={`button_${classType}`} onClick={actionOnClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Button from "./Button";
+import "./styles/modalStyle.css";
+interface ModalProps {
+  setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-const Modal: React.FC = () => {
+const Modal: React.FC<ModalProps> = ({ setOpenModal }) => {
   return (
     <div className="modal-backdrop">
-      <div></div>
+      <div className="modal-content-box">
+        <p>HOLA</p>
+        <Button
+          classType="button-modal"
+          actionOnClick={() => setOpenModal(false)}
+        />
+      </div>
     </div>
   );
 };
